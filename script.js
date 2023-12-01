00ae78315cc6ae9730ecb692eff3864b221a26a7
Full-Stack Developer <88977217+ryck-developer@users.noreply.github.com>
2023-12-01 17:59:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,4 @@
-const quiz = [
-  {
-    question:
-      'De acordo com o Artigo 1º do Regimento Interno da Assembleia Legislativa do estado do Piauí, assinale a alternativa correta sobre o local de funcionamento e a possibilidade de alteração desse local pela Assembleia Legislativa:',
-    answers: [
-      'O Artigo 1º estabelece que a Assembleia Legislativa do estado do Piauí funciona exclusivamente no Palácio Petrônio Portella, sem possibilidade de realização de atividades legislativas em outro local.',
-      'Conforme o Artigo 1º, a Assembleia Legislativa do estado do Piauí pode se reunir em outro edifício ou localidade somente mediante autorização do governador do estado.',
-      'O texto do Artigo 1º prevê que a Assembleia Legislativa do estado do Piauí pode funcionar no Palácio Petrônio Portella, e caso haja motivo relevante ou de força maior, a Assembleia pode se reunir em outro edifício ou localidade, com aprovação da Mesa e da maioria absoluta dos Deputados.',
-      'O Artigo 1º indica que a Assembleia Legislativa do estado do Piauí está autorizada a funcionar em qualquer edifício ou localidade, desde que seja aprovado por unanimidade dos Deputados.',
-      'Segundo o Artigo 1º do Regimento Interno, a Assembleia Legislativa do estado do Piauí pode funcionar no Palácio Petrônio Portella apenas durante os períodos ordinários, devendo se reunir em outro local durante sessões extraordinárias.',
-    ],
-    goodAnswer: 2, // Índice da resposta correta na array 'answers'
-  },
-  {
-    question: 'Question 2',
-    answers: ['answer1', 'answer2', 'answer3', 'answer4'],
-    goodAnswer: 1,
-  },
-];
+import quiz from './questions.js';
 let questionIndex = -1;
 const startBtn = document.querySelector('.start-button');
 const quizContainer = document.querySelector('.quiz-container');
