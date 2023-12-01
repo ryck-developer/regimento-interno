98ad83f0432d77ed703f59903776403a3fc62265
Full-Stack Developer <88977217+ryck-developer@users.noreply.github.com>
2023-12-01 17:55:22 -0300

    Update questions.js

--- questions.js
+++ questions.js
@@ -12,9 +12,15 @@ const quiz = [
     goodAnswer: 2, // Índice da resposta correta na array 'answers'
   },
   {
-    question: 'Question 2',
-    answers: ['answer1', 'answer2', 'answer3', 'answer4'],
-    goodAnswer: 1,
+    question: 'De acordo com o Artigo 2º do Regimento Interno da Assembleia Legislativa do estado do Piauí, assinale a alternativa correta sobre as sessões legislativas:',
+    answers: [
+      'A Assembleia Legislativa se reúne ordinariamente de 2 de fevereiro a 17 de julho e de 1º de agosto a 22 de dezembro, sendo que as reuniões marcadas para datas incluídas nesses períodos e que coincidam com sábados, domingos ou feriados são transferidas para o primeiro dia útil subsequente.',
+      'A sessão legislativa ordinária pode ser interrompida em 17 de julho, caso a Lei de Diretrizes Orçamentárias estadual não tenha sido aprovada, assim como em 22 de dezembro, se a Lei Orçamentária Anual do Estado ainda estiver pendente de aprovação.',
+      'A eleição da Mesa da Assembleia Legislativa não requer sessão preparatória.',
+      'A convocação de sessões extraordinárias é permitida apenas em situações de caráter excepcional e não segue um período específico, podendo ser convocada a qualquer momento.',
+      'A Assembleia Legislativa do estado do Piauí pode funcionar no Palácio Petrônio Portella apenas durante os períodos ordinários, devendo se reunir em outro local durante sessões extraordinárias.',
+    ],
+    goodAnswer: 1, // Índice da resposta correta na array 'answers'
   },
 ];
 
