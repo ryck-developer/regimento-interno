9296b208e3096c01de6a61d09dc6b4b142407a11
Full-Stack Developer <88977217+ryck-developer@users.noreply.github.com>
2023-12-02 06:09:37 -0300

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,15 +1,13 @@
 const quiz = [
   {
     question:
-      'De acordo com o Artigo 1º do Regimento Interno da Assembleia Legislativa do estado do Piauí, assinale a alternativa correta sobre o local de funcionamento e a possibilidade de alteração desse local pela Assembleia Legislativa:',
+      `Julgue o item a seguir de acordo com o Regimento Interno da Assembleia Legislativa do Piauí. 
+        Para tomar parte em qualquer discussão ou votar matéria de sua autoria, o Presidente da Assembleia Legislativa do Piauí transmite a presidência ao seu substituto.`,
     answers: [
-      'O Artigo 1º estabelece que a Assembleia Legislativa do estado do Piauí funciona exclusivamente no Palácio Petrônio Portella, sem possibilidade de realização de atividades legislativas em outro local.',
-      'Conforme o Artigo 1º, a Assembleia Legislativa do estado do Piauí pode se reunir em outro edifício ou localidade somente mediante autorização do governador do estado.',
-      'O texto do Artigo 1º prevê que a Assembleia Legislativa do estado do Piauí pode funcionar no Palácio Petrônio Portella, e caso haja motivo relevante ou de força maior, a Assembleia pode se reunir em outro edifício ou localidade, com aprovação da Mesa e da maioria absoluta dos Deputados.',
-      'O Artigo 1º indica que a Assembleia Legislativa do estado do Piauí está autorizada a funcionar em qualquer edifício ou localidade, desde que seja aprovado por unanimidade dos Deputados.',
-      'Segundo o Artigo 1º do Regimento Interno, a Assembleia Legislativa do estado do Piauí pode funcionar no Palácio Petrônio Portella apenas durante os períodos ordinários, devendo se reunir em outro local durante sessões extraordinárias.',
-    ],
-    goodAnswer: 2, 
+      'Certo',
+      'Errado',
+      ],
+    goodAnswer: 0, 
   },
   
   {
